1712c1f30154d1c05ae1152d5e0d92bbe287f89e
jvdionizio <dionizio145@gmail.com>
2022-07-14 16:39:19 -0300

    Fix: Price Filter

--- FinalProject/client-side/src/Components/FilterControl.js
+++ FinalProject/client-side/src/Components/FilterControl.js
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import Context from '../context/Context'
 import ChoseGenre from './ChoseGenre'
 import ChosePrice from './ChosePrice'
 import ChoseSort from './ChoseSort'
 
 export default function FilterControl () {
+
+  const {
+    excludeFilters,
+  } = useContext(Context);
+
+  const handleClick = () => {
+    excludeFilters();
+  };
+
   return (
     <div>
       <ChosePrice/>
       <ChoseGenre/>
       <ChoseSort/>
+      <button
+        onClick={handleClick}
+      >Limpar Filtros</button>
     </div>
   )
 }
